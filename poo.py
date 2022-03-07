edc0ab9facb1b7d83f485952d55bec13ff4d4905
Danielsjp <sjp.daniel@gmail.com>
2022-03-07 10:12:50 +0100

    prueba git

---
+++ poo.py
@@ -0,0 +1,13 @@
+#pruebas
+class Coche():
+         ruedas=4
+         largo=260
+         ancho=130
+
+         def arrancar(self):
+                  pass
+
+mazda=Coche() #ejemplar de clase
+
+a=mazda.ancho
+print (a)
