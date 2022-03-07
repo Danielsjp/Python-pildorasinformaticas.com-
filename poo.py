ecd900d7c154cc5e438e12dc3ed6408057bedd45
Danielsjp <sjp.daniel@gmail.com>
2022-03-07 11:18:28 +0100

    añadida un metodo de arranque en el coche

--- poo.py
+++ poo.py
@@ -3,11 +3,15 @@ class Coche():
          ruedas=4
          largo=260
          ancho=130
+         arrancado=False
 
          def arrancar(self):
-                  pass
+                  Coche.arrancado=True
 
 mazda=Coche() #ejemplar de clase
 
 a=mazda.ancho
 print (a)
+print (mazda.arrancado) #aqui el coche no esta arrancado
+mazda.arrancar() #arrancamos coche
+print (mazda.arrancado) #devuelve true, el coche esta arrancado.
